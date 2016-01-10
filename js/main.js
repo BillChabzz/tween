3a924fcef29b6bbb0b30854485a5419edbae0cb7
BillChabzz <chaby9.8@gmail.com>
2016-01-10 13:33:33 +0300

    adding fallback funtion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+//TweenMax(target,duration,{vars}) different methods may add additoinal fallbacks
 TweenMax.from(".logo", 2, {opacity:0,scale: 0, ease:Bounce.easeOut
                         });
-TweenMax.staggerFrom(".box", 1, {opacity: 0, y: 200,delay: 1,rotation: 360, scale:2}, 0.2 )
+TweenMax.staggerFrom(".box", 1, {opacity: 0, y: 200,delay: 1,rotation: 360, scale:2}, 0.2 );
+//Once animation is done fade out content
+TweenMax.to(".logo, .box",0.5, {opacity:0, delay: 3, onComplete: complete});
+
+//fallback funtion
+function complete(){
+    alert("Wow, very ameze such animate much done!")
+}
