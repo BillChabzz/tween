039d637c5b673a6eaa214f05d5f2ac053ead9836
BillChabzz <chaby9.8@gmail.com>
2016-01-15 22:10:02 +0300

    more tweens

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-//TweenMax(target,duration,{vars}) different methods may add additoinal fallbacks
+var element = document.getElementById("element");
+var photo = document.getElementById("photo");// can also use jquery's $("photo")
+TweenLite.fromTo(photo, 1.5, {width:0, height:0}, {width:100, height:200});
+TweenLite.to(element, 1 ,{padding: 5, 
+                          backgroundColor: "#ff2d6f",
+                          rotation: 10,
+                         ease:Power2.easeOut});
+
+//TweenMax.(tweenMethod)(target,duration,{vars}) different methods may add additoinal fallbacks
 TweenMax.from(".logo", 2, {opacity:0,scale: 0, ease:Bounce.easeOut
                         });
 TweenMax.staggerFrom(".box", 1, {opacity: 0, y: 200,delay: 1,rotation: 360, scale:2}, 0.2 );
 //Once animation is done fade out content
-TweenMax.to(".logo, .box",0.5, {opacity:0, delay: 3, onComplete: complete});
+//TweenMax.to(".logo, .box",0.5, {opacity:0, delay: 3, onComplete: complete});
 
 //fallback funtion
-function complete(){
-    alert("Wow, very ameze such animate much done!")
-}
+//function complete(){
+//    alert("Wow, very amaze such animate much done!")
+//}
